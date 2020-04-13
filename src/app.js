5390246f9b9f5e3ab766bfa5f79a259ae66379ab
Shubham More <shubhammore1796@gmail.com>
2020-04-13 12:27:50 +0530

    sportizen-backend

--- src/app.js
+++ src/app.js
@@ -3,6 +3,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const compression = require('compression');
 var cors = require('cors');
+const fs = require('fs');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const morgan = require('morgan');
 
 require('./database/mongoose');
 
@@ -19,13 +26,53 @@ app.use(express.json());
 app.use(cors());
 app.use(compression());
 
+app.use('/log', express.static(path.join('log')));
+app.use('/images', express.static(path.join('images')));
 app.use('/fileToUpload', express.static(path.join('fileToUpload')));
-
-// CONTENT
-app.use('/image-categories', express.static(path.join('image-categories')));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+
+// NOSQL INJECTION
+app.use(mongoSanitize());
+
+// to replace prohibited characters with '_':
+app.use(
+  mongoSanitize({
+    replaceWith: '_'
+  })
+);
+
+// Prevents HTML Code in Input Fields
+app.use(xss());
+
+// Prevents HTTP Parameter Pollution, i.e. Duplicate Parameter in Query String
+app.use(hpp());
+
+// Helmet helps you secure your Express apps by setting various HTTP headers
+app.use(helmet());
+
+// HTTP request logger
+app.use(
+  morgan('combined', {
+    stream: fs.createWriteStream(
+      path.join(__dirname, '..', 'log', 'access.log'),
+      {
+        flags: 'a'
+      }
+    )
+  })
+);
+
+// To Limit Incomming request from same IP
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 100 // limit each IP to 100 requests per windowMs
+});
+
+//  apply to all requests
+app.use(limiter);
 
 app.use((req, res, next) => {
   // Website you wish to allow to connect
