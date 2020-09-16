8a2f39c456ac97e94c1d93143bd96bc90efd853a
Shubham More <shubhammore1796@gmail.com>
2020-09-16 15:03:19 +0530

    sportizen

--- src/models/user-profile.model.js
+++ src/models/user-profile.model.js
@@ -23,6 +23,14 @@ const userProfileSchema = new mongoose.Schema({
     type: String,
     default: '0',
   },
+  phoneNo: {
+    type: String,
+    required: true,
+  },
+  story: {
+    type: String,
+    default: null,
+  },
   sportsInterest: [String],
   friends: [],
   followers: [],
