3ff495ab88868af3eaba6638d09e778858643208
Shubham More <shubhammore1796@gmail.com>
2020-09-26 18:00:05 +0530

    sportizen

--- src/handlers/user-profile/saveUserStory.js
+++ src/handlers/user-profile/saveUserStory.js
@@ -2,12 +2,14 @@ const UserProfile = require('../../models/user-profile.model');
 
 const saveUserStory = async (req, res) => {
   try {
+    console.log(req.body);
     const userProfile = await UserProfile.findByIdAndUpdate(req.body._id, {
       story: req.body.story,
     });
 
     res.send(userProfile);
   } catch (e) {
+    console.log(e);
     let err = '' + e;
     res.status(400).send(err.replace('Error: ', ''));
   }
