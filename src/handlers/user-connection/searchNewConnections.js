efb1281b2ad2377dcd7db114f7b3b2c1bb1d33fc
Shubham More <shubhammore1796@gmail.com>
2020-09-26 21:46:30 +0530

    sportizen

--- src/handlers/user-connection/searchNewConnections.js
+++ src/handlers/user-connection/searchNewConnections.js
@@ -4,7 +4,10 @@ const searchNewConnections = async (req, res) => {
   try {
     const name = new RegExp('.*' + req.body.searchName + '.*');
 
-    const userConnections = await UserProfile.find({ name });
+    const userConnections = await UserProfile.find(
+      { name },
+      { _id: 1, name: 1, email: 1, userImageURL: 1 }
+    );
 
     res.send(userConnections);
   } catch (e) {
