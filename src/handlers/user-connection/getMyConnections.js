f61930219f00e87748f33b23275e195a8cab43eb
Akash Kulkarni <akashkulkarni796@gmail.com>
2020-09-29 23:20:13 +0530

    conn model

--- src/handlers/user-connection/getMyConnections.js
+++ src/handlers/user-connection/getMyConnections.js
@@ -8,10 +8,52 @@ const getMyConnections = async (req, res) => {
       status: 'connected',
     });
 
+    const connData = await UserConnection.aggregate([
+      {
+        $match:{
+          $expr:{
+            $or:[
+              {
+                $eq:['$firstUser',req.user._id]
+              },{
+                $eq:['$secondUser',req.user._id]
+              }
+            ]
+          },
+          status:'connected'
+        }
+      },{
+        $addFields:{
+          searchUser:{
+            $cond:{
+              if:{
+                $eq:['$firstUser',req.user._id]
+              }, 
+              then:'$secondUser',
+              else:'$firstUser'
+            }
+          }
+        }  
+      },{
+        $addFields:{
+          searchUser:{
+            $toObjectId:'$searchUser'
+          }
+        }
+      },{
+        $lookup:{
+          from:'UserProfile',
+          localField:'searchUser',
+          foreignField:'_id',
+          as:'connectionDetails'
+        }
+      }
+    ])
+
     if (!userConnection) {
       throw new Error('');
     }
-    res.send(userConnection);
+    res.send(connData);
   } catch (e) {
     let err = '' + e;
     res.status(400).send(err.replace('Error: ', ''));
