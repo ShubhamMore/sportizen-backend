a026f0baa2fa0106bef00891303f5763560b15d3
Shubham More <shubhammore1796@gmail.com>
2020-10-04 12:45:39 +0530

    sportizen

--- src/handlers/user-connection/getMyConnections.js
+++ src/handlers/user-connection/getMyConnections.js
@@ -1,59 +1,100 @@
 const UserConnection = require('../../models/user-connection.model');
-const UserProfile = require('../../models/user-profile.model');
 
 const getMyConnections = async (req, res) => {
   try {
-    const userConnection = await UserConnection.find({
-      $or: [{ firstUser: req.user._id }, { secondUser: req.user._id }],
-      status: 'connected',
-    });
+    // const userConnection = await UserConnection.find({
+    //   $or: [{ firstUser: req.user._id }, { secondUser: req.user._id }],
+    //   status: 'connected',
+    // });
+
+    // const connData = await UserConnection.aggregate([
+    //   {
+    //     $match:{
+    //       $expr:{
+    //         $or:[
+    //           {
+    //             $eq:['$firstUser',req.user._id]
+    //           },{
+    //             $eq:['$secondUser',req.user._id]
+    //           }
+    //         ]
+    //       },
+    //       status:'connected'
+    //     }
+    //   },{
+    //     $addFields:{
+    //       searchUser:{
+    //         $cond:{
+    //           if:{
+    //             $eq:['$firstUser',req.user._id]
+    //           },
+    //           then:'$secondUser',
+    //           else:'$firstUser'
+    //         }
+    //       }
+    //     }
+    //   },{
+    //     $addFields:{
+    //       searchUser:{
+    //         $toObjectId:'$searchUser'
+    //       }
+    //     }
+    //   },{
+    //     $lookup:{
+    //       from:'UserProfile',
+    //       localField:'searchUser',
+    //       foreignField:'_id',
+    //       as:'connectionDetails'
+    //     }
+    //   }
+    // ])
 
     const connData = await UserConnection.aggregate([
       {
-        $match:{
-          $expr:{
-            $or:[
+        $match: {
+          $expr: {
+            $or: [
               {
-                $eq:['$firstUser',req.user._id]
-              },{
-                $eq:['$secondUser',req.user._id]
-              }
-            ]
+                $eq: ['$firstUser', req.user.email],
+              },
+              {
+                $eq: ['$secondUser', req.user.email],
+              },
+            ],
           },
-          status:'connected'
-        }
-      },{
-        $addFields:{
-          searchUser:{
-            $cond:{
-              if:{
-                $eq:['$firstUser',req.user._id]
-              }, 
-              then:'$secondUser',
-              else:'$firstUser'
-            }
-          }
-        }  
-      },{
-        $addFields:{
-          searchUser:{
-            $toObjectId:'$searchUser'
-          }
-        }
-      },{
-        $lookup:{
-          from:'UserProfile',
-          localField:'searchUser',
-          foreignField:'_id',
-          as:'connectionDetails'
-        }
-      }
-    ])
+          status: 'connected',
+        },
+      },
+      {
+        $addFields: {
+          searchUser: {
+            $cond: {
+              if: {
+                $eq: ['$firstUser', req.user.email],
+              },
+              then: '$secondUser',
+              else: '$firstUser',
+            },
+          },
+        },
+      },
+      {
+        $lookup: {
+          from: 'userprofiles',
+          localField: 'searchUser',
+          foreignField: 'email',
+          as: 'connectionDetails',
+        },
+      },
+      {
+        $replaceRoot: {
+          newRoot: { $mergeObjects: [{ $arrayElemAt: ['$connectionDetails', 0] }, '$$ROOT'] },
+        },
+      },
+      { $project: { connectionDetails: 0 } },
+      { $project: { _id: 1, email: 1, name: 1, userImageURL: 1, userCoverImageURL: 1 } },
+    ]);
 
-    if (!userConnection) {
-      throw new Error('');
-    }
-    
     res.send(connData);
   } catch (e) {
     let err = '' + e;
