72a0e871f9895fbc2a9580b10ff3430e40ffc59c
Akash Kulkarni <akashkulkarni796@gmail.com>
2020-11-05 22:35:47 +0530

    search user API

--- src/controllers/user-connection-controller/searchNewConnections.js
+++ src/controllers/user-connection-controller/searchNewConnections.js
@@ -6,61 +6,61 @@ const responseHandler = require('../../handlers/response.handler');
 const searchNewConnections = async (req, res) => {
   try {
     const name = new RegExp('.*' + req.body.searchName + '.*');
-
-    const userConnections = await UserProfile.aggregate([
-      {
-        $match: {
-          name,
-        },
-      },
-      { $project: { _id: 1, name: 1, email: 1, userImageURL: 1 } },
-      { $addFields: { connectionId: { $toString: _id } } },
-      {
-        $lookup: {
-          from: 'userconnections',
-          let: { connectionId: '$connectionId' },
-          pipeline: [
-            {
-              $match: {
-                $expr: {
-                  $and: [
-                    {
-                      $eq: ['$primaryUser', req.user._id],
-                    },
-                    {
-                      $eq: ['$followedUser', '$$connectionId'],
-                    },
-                  ],
-                },
-              },
-            },
-            {
-              $cond: {
-                if: {
-                  $eq: ['$status', 'following'],
-                },
-                then: 'following',
-                else: {
-                  $match: {
-                    $expr: {
-                      $and: [
-                        {
-                          $eq: ['$primaryUser', '$$connectionId'],
-                        },
-                        {
-                          $eq: ['$followedUser', req.user._id],
-                        },
-                      ],
-                    },
-                  },
-                },
-              },
-            },
-          ],
-          as: 'connection',
-        },
-      },
-    ]);
+    const userConnections = await UserProfile.aggregate([{ $match: { name } }]);
+    // const userConnections = await UserProfile.aggregate([
+    //   {
+    //     $match: {
+    //       name,
+    //     },
+    //   },
+    //   { $project: { _id: 1, name: 1, email: 1, userImageURL: 1 } },
+    //   { $addFields: { connectionId: { $toString: _id } } },
+    //   {
+    //     $lookup: {
+    //       from: 'userconnections',
+    //       let: { connectionId: '$connectionId' },
+    //       pipeline: [
+    //         {
+    //           $match: {
+    //             $expr: {
+    //               $and: [
+    //                 {
+    //                   $eq: ['$primaryUser', req.user._id],
+    //                 },
+    //                 {
+    //                   $eq: ['$followedUser', '$$connectionId'],
+    //                 },
+    //               ],
+    //             },
+    //           },
+    //         },
+    //         {
+    //           $cond: {
+    //             if: {
+    //               $eq: ['$status', 'following'],
+    //             },
+    //             then: 'following',
+    //             else: {
+    //               $match: {
+    //                 $expr: {
+    //                   $and: [
+    //                     {
+    //                       $eq: ['$primaryUser', '$$connectionId'],
+    //                     },
+    //                     {
+    //                       $eq: ['$followedUser', req.user._id],
+    //                     },
+    //                   ],
+    //                 },
+    //               },
+    //             },
+    //           },
+    //         },
+    //       ],
+    //       as: 'connection',
+    //     },
+    //   },
+    // ]);
 
     responseHandler(userConnections, 200, res);
   } catch (e) {
