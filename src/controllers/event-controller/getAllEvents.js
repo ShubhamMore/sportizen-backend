e1688d0f96a8e4045510db29929c0f49e15baa43
Shubham More <shubhammore1796@gmail.com>
2020-12-09 12:47:53 +0530

    sportizen

--- src/controllers/event-controller/getAllEvents.js
+++ src/controllers/event-controller/getAllEvents.js
@@ -5,9 +5,18 @@ const responseHandler = require('../../handlers/response.handler');
 
 const getAllEvents = async (req, res) => {
   try {
+    const query = {};
+
+    // if (req.body.longitude && req.body.latitude) {
+    //   query.location = {
+    //     $geoWithin: { $centerSphere: [[req.body.latitude, req.body.longitude], 100 / 3963.2] },
+    //     // 10 Miles of Radius, The query converts the distance to radians by dividing by the approximate equatorial radius of the earth, 3963.2 miles
+    //   };
+    // }
+
     const events = await Event.aggregate([
       {
-        $match: {},
+        $match: query,
       },
       {
         $addFields: {
