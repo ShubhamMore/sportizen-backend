7631a7fe798f52cd008057f7276d7f2dd19b17c7
Shubham More <shubhammore1796@gmail.com>
2021-02-01 23:02:41 +0530

    class-master

--- src/controllers/post-controller/createPost.js
+++ src/controllers/post-controller/createPost.js
@@ -54,132 +54,6 @@ const createPost = async (req, res) => {
         },
       },
       {
-        $addFields: {
-          id: {
-            $toString: '$_id',
-          },
-        },
-      },
-      {
-        $lookup: {
-          from: 'postlikes',
-          let: { postId: '$id' },
-          pipeline: [
-            {
-              $match: {
-                $expr: {
-                  $and: [
-                    { $eq: ['$post', '$$postId'] },
-                    { $eq: ['$sportizenUser', req.user.sportizenId] },
-                  ],
-                },
-              },
-            },
-            {
-              $project: {
-                _id: 0,
-                alreadyLiked: { $cond: [{ $eq: ['$postLike', true] }, true, false] },
-              },
-            },
-          ],
-          as: 'likeStatus',
-        },
-      },
-      {
-        $lookup: {
-          from: 'postlikes',
-          let: { postId: '$id' },
-          pipeline: [
-            { $match: { $expr: { $eq: ['$post', '$$postId'] } } },
-            { $count: 'postLikes' },
-          ],
-          as: 'likes',
-        },
-      },
-      {
-        $lookup: {
-          from: 'postviews',
-          let: { postId: '$id' },
-          pipeline: [
-            {
-              $match: {
-                $expr: {
-                  $and: [
-                    { $eq: ['$post', '$$postId'] },
-                    { $eq: ['$sportizenUser', req.user.sportizenId] },
-                  ],
-                },
-              },
-            },
-            {
-              $project: {
-                _id: 0,
-                alreadyViewed: { $cond: [{ $eq: ['$postView', true] }, true, false] },
-              },
-            },
-          ],
-          as: 'viewStatus',
-        },
-      },
-      {
-        $lookup: {
-          from: 'postviews',
-          let: { postId: '$id' },
-          pipeline: [
-            { $match: { $expr: { $eq: ['$post', '$$postId'] } } },
-            { $count: 'postViews' },
-          ],
-          as: 'views',
-        },
-      },
-      {
-        $lookup: {
-          from: 'comments',
-          let: { postId: '$id' },
-          pipeline: [
-            { $match: { $expr: { $eq: ['$post', '$$postId'] } } },
-            { $count: 'postComments' },
-          ],
-          as: 'comments',
-        },
-      },
-      {
-        $lookup: {
-          from: 'replycomments',
-          let: { postId: '$id' },
-          pipeline: [
-            { $match: { $expr: { $eq: ['$post', '$$postId'] } } },
-            { $count: 'postReplyComments' },
-          ],
-          as: 'replyComments',
-        },
-      },
-      {
-        $lookup: {
-          from: 'saveposts',
-          let: { postId: '$id' },
-          pipeline: [
-            {
-              $match: {
-                $expr: {
-                  $and: [
-                    { $eq: ['$post', '$$postId'] },
-                    { $eq: ['$sportizenUser', req.user.sportizenId] },
-                  ],
-                },
-              },
-            },
-            {
-              $project: {
-                _id: 0,
-                alreadySaved: { $cond: [{ $eq: ['$savePost', true] }, true, false] },
-              },
-            },
-          ],
-          as: 'savePosts',
-        },
-      },
-      {
         $lookup: {
           from: 'userprofiles',
           let: { sportizenUserId: '$sportizenUser' },
@@ -200,53 +74,11 @@ const createPost = async (req, res) => {
       },
       {
         $replaceRoot: {
-          newRoot: { $mergeObjects: [{ $arrayElemAt: ['$likeStatus', 0] }, '$$ROOT'] },
-        },
-      },
-      {
-        $replaceRoot: {
-          newRoot: { $mergeObjects: [{ $arrayElemAt: ['$likes', 0] }, '$$ROOT'] },
-        },
-      },
-      {
-        $replaceRoot: {
-          newRoot: { $mergeObjects: [{ $arrayElemAt: ['$viewStatus', 0] }, '$$ROOT'] },
-        },
-      },
-      {
-        $replaceRoot: {
-          newRoot: { $mergeObjects: [{ $arrayElemAt: ['$views', 0] }, '$$ROOT'] },
-        },
-      },
-      {
-        $replaceRoot: {
-          newRoot: { $mergeObjects: [{ $arrayElemAt: ['$comments', 0] }, '$$ROOT'] },
-        },
-      },
-      {
-        $replaceRoot: {
-          newRoot: { $mergeObjects: [{ $arrayElemAt: ['$replyComments', 0] }, '$$ROOT'] },
-        },
-      },
-      {
-        $replaceRoot: {
-          newRoot: { $mergeObjects: [{ $arrayElemAt: ['$savePosts', 0] }, '$$ROOT'] },
-        },
-      },
-      {
-        $replaceRoot: {
           newRoot: { $mergeObjects: [{ $arrayElemAt: ['$postUser', 0] }, '$$ROOT'] },
         },
       },
       {
         $project: {
-          likeStatus: 0,
-          likes: 0,
-          viewStatus: 0,
-          views: 0,
-          comments: 0,
-          replyComments: 0,
-          savePosts: 0,
           postUser: 0,
           __v: 0,
         },
